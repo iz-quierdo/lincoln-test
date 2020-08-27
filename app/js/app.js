6b162567e06893575798efb04cfd92eedcbeef3d
Alexander Smirnov <alex-wd@yandex.ru>
2020-08-28 02:16:25 +0300

    Add responsive display to slick-slider

--- app/js/app.js
+++ app/js/app.js
@@ -1,17 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
 
-document.addEventListener("DOMContentLoaded", function() {
-
-  ;(function($) {
+  ;(function ($) {
 
     $('.slick').slick({
       infinite: true,
       arrows: false,
       dots: true,
-      customPaging: function(slider, i) {
+      customPaging: function (slider, i) {
         return '<div class="slider-dot"></div>';
       },
       slidesToShow: 2,
       slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 960,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        }
+      ]
 
     });
 
